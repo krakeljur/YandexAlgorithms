bb6472d988f8972a477f6832a470e4e676dbc5f6
krakeljur <125911670+krakeljur@users.noreply.github.com>
2023-11-25 19:22:37 +0300

    add new solution

--- src/main/kotlin/FourthHomeWork.kt
+++ src/main/kotlin/FourthHomeWork.kt
@@ -1,4 +1,3 @@
-import sun.security.ec.point.ProjectivePoint.Mutable
 
 fun rearrangement() {
 
@@ -95,24 +94,58 @@ fun reqPositionInZoo(
     return result
 }
 
-// ДОДЕЛАТЬ
 fun maxCut() {
 
     val n = readln().trim().toInt()
-    val inSecondTeam = MutableList(n) {false}
+    val inSecondTeam = MutableList(n) { 1 }
     val matrix = mutableListOf<MutableList<Int>>()
-    var maxWeight = 0
 
     for (i in 1..n) {
         matrix.add(readln().trim().split(" ").map { it.toInt() }.toMutableList())
     }
-    
 
+    val maxWeight = reqMaxCut(inSecondTeam, n, matrix)
 
+    println(maxWeight.first)
+    println(maxWeight.second.joinToString(" "))
 }
 
-//СДЕЛАТЬ
-/*fun courier(){
+fun reqMaxCut(
+    inSecondTeam: MutableList<Int>,
+    n: Int, matrix: MutableList<MutableList<Int>>,
+    cur: Int = 0,
+    curI: Int = 1
+): Pair<Int, List<Int>> {
+    var maxWeight = 0
+    var ansList = inSecondTeam.toList()
+    var weightCurrent = cur
+    for (i in curI..<n) {
+        if (inSecondTeam[i] == 1) {
+            inSecondTeam[i] = 2
+            for (j in 0..<n) {
+                if (inSecondTeam[j] == 1 && j != i) {
+                    weightCurrent += matrix[i][j]
+                } else if (inSecondTeam[j] == 2 && j != i) {
+                    weightCurrent -= matrix[i][j]
+                }
+            }
+            val nextReq = reqMaxCut(inSecondTeam, n, matrix, weightCurrent, i)
+            if (nextReq.first > maxWeight) {
+                maxWeight = nextReq.first
+                ansList = nextReq.second
+            }
+            inSecondTeam[i] = 1
+            weightCurrent = cur
+        }
+    }
+    return if (maxWeight > weightCurrent)
+        Pair(maxWeight, ansList)
+    else
+        Pair(weightCurrent, inSecondTeam.toMutableList())
+}
+
+
+fun courier() {
     val n = readln().trim().toInt()
     val matrix = mutableListOf<List<Int>>()
 
@@ -120,19 +153,56 @@ fun maxCut() {
         matrix.add(readln().trim().split(" ").map { it.toInt() })
     }
 
-    val visited = MutableList(n) {false}
+    if (n != 1) {
 
-    var min = 0
+        val allVariants = mutableListOf<String>()
+        val prevs = mutableListOf<Int>()
+        val isVisited = MutableList(n + 1) { false }
+        isVisited[0] = true
+        isVisited[1] = true
 
-    min = courierReq(min, visited, matrix)
-
-    println("$min")
+        val min = reqCourier(prevs, allVariants, n - 1, isVisited, matrix)
+        println("${if (min == 0) -1 else min}")
+    } else println(0)
 }
 
-fun courierReq(baseMin: Int, visited : MutableList<Boolean>, roads: MutableList<List<Int>>) : Int {
+fun reqCourier(
+    prevs: MutableList<Int>,
+    res: MutableList<String>,
+    n: Int,
+    isUse: MutableList<Boolean>,
+    matrix: MutableList<List<Int>>
+) : Int {
+    var min = 0
+    for (i in 2..<isUse.size) {
+
+        if (!isUse[i]) {
+            isUse[i] = true
+            prevs.add(i-1)
+            if (prevs.size == isUse.size - 2) {
+                val newWay = prevs.joinToString("")
+                var thisWay = matrix[0][newWay[0].digitToInt()]
+                if (thisWay != 0) {
+                    for (i in 1..<newWay.length) {
+                        if (matrix[newWay[i - 1].digitToInt()][newWay[i].digitToInt()] != 0)
+                            thisWay += matrix[newWay[i - 1].digitToInt()][newWay[i].digitToInt()]
+                        else {
+                            return 0
+                        }
+                    }
+                        thisWay += matrix[newWay.last().digitToInt()][0]
+                    if ((min < thisWay && min == 0) || (min > thisWay))
+                        min = thisWay
+                }
+            }
+            val nextReq = reqCourier(prevs, res, n - 1, isUse, matrix)
+            if (nextReq != 0 && ((nextReq < min && min != 0) || (nextReq > min && min == 0)))
+                min = nextReq
+            prevs.removeLast()
+            isUse[i] = false
+        }
+    }
+    return min
+}
 
 
-
-
-    return 0
-}*/
